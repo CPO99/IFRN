eb7e8815ea685e4b8f57f256169a8ba6ea9ddc0c
Clenylson Perycles <clenylson.perycles@gmail.com>
2024-11-23 16:40:08 -0300

    Adicao de comentarios ao codigo

--- Primeiro_Semestre/Programacao_Para_Redes/Atividade_Avaliativa_1/q2.py
+++ Primeiro_Semestre/Programacao_Para_Redes/Atividade_Avaliativa_1/q2.py
@@ -4,11 +4,11 @@ N1 = float(input("Informe a nota 1: "))
 N2 = float(input("Informe a nota 2: "))
 
 
-MEDIA = (2*N1 + 3*N2) / 5
+MEDIA = (2*N1 + 3*N2) / 5 #primeiro calculo da media
 
-SITUACAO = "APROVADO"
+SITUACAO = "APROVADO" #considerando por padrão como aprovado
 
-if 20<=MEDIA<60:
+if 20<=MEDIA<60: #verificando se a media necessita de prova final
     print("\nMEDIA ATUAL:",MEDIA)
     NF = float(input("\nInforme a nota da prova final: "))
     MDF1 = (MEDIA + NF) / 2
@@ -17,16 +17,16 @@ if 20<=MEDIA<60:
 
     MDF3 = (2*N1 + 3*NF) / 5
 
-    if MDF1 > MDF2 and MDF1 > MDF3:
+    if MDF1 > MDF2 and MDF1 > MDF3: #segundo calculo de media
         MEDIA = MDF1
-    elif MDF2 > MDF1 and MDF2 > MDF3:
+    elif MDF2 > MDF1 and MDF2 > MDF3: #terceiro calculo de media
         MEDIA = MDF2
     else:
         MEDIA = MDF3
 
     if MEDIA < 60:
         SITUACAO = "REPROVADO"
-elif 20>MEDIA:
+elif 20>MEDIA: #reprovação direta, primeira media abaixo de 20
     SITUACAO = "REPROVADO"
 
 print("\nSituação:",SITUACAO)
