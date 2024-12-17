d9c0f161142e73e8cf319d3ab9f2fb50766cec23
Clenylson Perycles <clenylson.perycles@gmail.com>
2024-12-17 20:26:48 -0300

    inserido desvio padrão da média calculada

--- Primeiro_Semestre/Programacao_Para_Redes/Questões em sala/Calculador_de_media.py
+++ Primeiro_Semestre/Programacao_Para_Redes/Questões em sala/Calculador_de_media.py
@@ -5,13 +5,25 @@ NUMERO = 0
 SOMA = 0
 CONT = -1 #contador iniciando negativo para compensar a contagem do número negativo de parada
 
+NUMEROS = []
+
 while NUMERO >= 0:
     SOMA += NUMERO
     CONT += 1
     
     NUMERO = int(input("Informe um número: "))
 
-    
+    if NUMERO >= 0:
+        NUMEROS.append(NUMERO)
 
+SOMA_DP = 0
+
+print (NUMEROS)
+
+for num in NUMEROS:
+    print ((num - (SOMA / CONT))**2)
+    SOMA_DP += (num - (SOMA / CONT))**2
+    
 print("\nQuantidade de números informados:",CONT)
 print("Média dos número informados:",SOMA / CONT if SOMA != 0 else "sem dados")
+print("Desvio padrão:",(SOMA_DP /(CONT - 1)) ** (1/2))
