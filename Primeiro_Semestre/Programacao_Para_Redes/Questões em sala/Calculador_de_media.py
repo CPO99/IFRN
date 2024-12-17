874088325cdaa9bfbb27afc2c649da524ba41a03
Clenylson Perycles <clenylson.perycles@gmail.com>
2024-12-17 19:17:51 -0300

    Questão do calculador de média

---
+++ Primeiro_Semestre/Programacao_Para_Redes/Questões em sala/Calculador_de_media.py
@@ -0,0 +1,17 @@
+print("CALCULANDO MÉDIA ARITMÉTICA DE SÉRIE DE NÚMEROS")
+print("- Informe um número negativo para parar\n")
+
+NUMERO = 0
+SOMA = 0
+CONT = -1 #contador iniciando negativo para compensar a contagem do número negativo de parada
+
+while NUMERO >= 0:
+    SOMA += NUMERO
+    CONT += 1
+    
+    NUMERO = int(input("Informe um número: "))
+
+    
+
+print("\nQuantidade de números informados:",CONT)
+print("Média dos número informados:",SOMA / CONT)
