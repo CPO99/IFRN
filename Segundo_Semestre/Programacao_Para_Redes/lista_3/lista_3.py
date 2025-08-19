e5e5ba197cb011ee7cf00eacedf124fa5b5999f2
Clenylson Perycles <clenylson.perycles@gmail.com>
2025-08-19 11:47:43 -0300

    ajuste no tratamento da URL

--- Segundo_Semestre/Programacao_Para_Redes/lista_3/lista_3.py
+++ Segundo_Semestre/Programacao_Para_Redes/lista_3/lista_3.py
@@ -25,25 +25,30 @@ def extrairURL(url:str):
             return (sucesso, protocolo, host, caminho, arquivoNome)
 
         #extraindo caminho
-        try:
-            caminho = url[fimHost + 1:]
-        except:
+        
+        if url.find("/",fimHost) != -1:
+            caminho = url[url.find("/",fimHost):]
+
+            if caminho == "/":
+                caminho = ""
+        else:
             caminho = ""
+
+            
+            
 
         #extraindo nome do arquivo
         arquivoNome = url.split('/')[-1]
         if arquivoNome.find(".") == -1 or caminho == "":
             arquivoNome = ""
-        else:
-            caminho = caminho.split(arquivoNome)[0]
 
         #verificando protocolo
-        if url.find("http") != -1:
-            protocolo = "http"
+        if url.find("https") != -1:
+            protocolo = "https"
             sucesso=True
             return (sucesso, protocolo, host, caminho, arquivoNome)
-        elif url.find("https") != -1:
-            protocolo = "https"
+        elif url.find("http") != -1:
+            protocolo = "http"
             sucesso=True
             return (sucesso, protocolo, host, caminho, arquivoNome)
         else:
@@ -54,14 +59,13 @@ def extrairURL(url:str):
         return (sucesso, protocolo, host, caminho, arquivoNome)
 
 ver = True
-
 while ver:
     url = input("Informe a URL completa: ")
 
     retorno = extrairURL(url)
     if retorno[0] == False:
-        print("Informe uma URL válida no padrão protocolo+host+caminho+arquivo\n" \
-        "Exemplo: https://google.com.br/index.html")
+        print("Informe uma URL válida."\
+              "\n\nExemplo: \n - https://google.com\n - https://google.com/caminho \n - https://google.com/caminho/index.html\n\n")
     else:
         print (retorno)
         ver = False
