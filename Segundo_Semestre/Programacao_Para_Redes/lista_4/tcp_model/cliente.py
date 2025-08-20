6bbeb5898770fd9932e92aa1f40f8277e0e46a01
Clenylson Perycles <clenylson.perycles@gmail.com>
2025-08-20 17:00:57 -0300

    implementado uma versão do settimeout em utp

---
+++ Segundo_Semestre/Programacao_Para_Redes/lista_4/tcp_model/cliente.py
@@ -0,0 +1,35 @@
+import socket
+
+from constantes import *
+
+# Criando o socket (socket.AF_INET -> IPV4 / socket.SOCK_STREAM -> UTP)
+sockUDP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print('\n\nPara sair digite EXIT...\n\n')
+
+while True:
+   try:
+      # Informando a mensagem a ser enviada para o servidor
+      strMensagem = input('Digite a mensagem: ')
+
+      # Saindo do Cliente quando digitar EXIT
+      if strMensagem.lower().strip() == 'exit': break
+
+      # Enviando a mensagem ao servidor      
+      sockUDP.sendto(strMensagem.encode(CODE_PAGE), (HOST_IP_SERVER, HOST_PORT))
+
+      # Recebendo ECHO do servidor
+      byteRetorno, tuplaRetorno = sockUDP.recvfrom(BUFFER_SIZE)
+
+      print (f'ECHO RECEBIDO {tuplaRetorno}: {byteRetorno.decode(CODE_PAGE)} ')
+
+      # verificando mensagem de finalização
+      byteRetorno, tuplaRetorno = sockUDP.recvfrom(BUFFER_SIZE)
+
+      if byteRetorno.decode(CODE_PAGE) == "EXIT_BYE":
+         print("Encerrando aplicação por ordem do servidor...")
+         break
+   except:
+      print("\nOcorreu um erro inesperado")
+# Fechando o socket
+sockUDP.close()
