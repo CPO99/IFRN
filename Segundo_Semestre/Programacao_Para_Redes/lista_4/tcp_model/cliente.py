fb7a1d96816b3d8833203057e24ac2a1ac127dc1
Clenylson Perycles <clenylson.perycles@gmail.com>
2025-08-20 17:08:14 -0300

    ajustado cliente. trocado trechos de código udp por tcp

--- Segundo_Semestre/Programacao_Para_Redes/lista_4/tcp_model/cliente.py
+++ Segundo_Semestre/Programacao_Para_Redes/lista_4/tcp_model/cliente.py
@@ -1,35 +1,42 @@
 import socket
-
 from constantes import *
 
-# Criando o socket (socket.AF_INET -> IPV4 / socket.SOCK_STREAM -> UTP)
-sockUDP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Criando o socket TCP (AF_INET -> IPV4 / SOCK_STREAM -> TCP)
+sockTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Conectando ao servidor
+sockTCP.connect((HOST_IP_SERVER, HOST_PORT))
 
 print('\n\nPara sair digite EXIT...\n\n')
 
 while True:
-   try:
-      # Informando a mensagem a ser enviada para o servidor
-      strMensagem = input('Digite a mensagem: ')
+    try:
+        # Informando a mensagem a ser enviada para o servidor
+        strMensagem = input('Digite a mensagem: ')
 
-      # Saindo do Cliente quando digitar EXIT
-      if strMensagem.lower().strip() == 'exit': break
+        # Saindo do Cliente quando digitar EXIT
+        if strMensagem.lower().strip() == 'exit':
+            break
 
-      # Enviando a mensagem ao servidor      
-      sockUDP.sendto(strMensagem.encode(CODE_PAGE), (HOST_IP_SERVER, HOST_PORT))
+        # Enviando a mensagem ao servidor
+        sockTCP.send(strMensagem.encode(CODE_PAGE))
 
-      # Recebendo ECHO do servidor
-      byteRetorno, tuplaRetorno = sockUDP.recvfrom(BUFFER_SIZE)
+        # Recebendo ECHO do servidor
+        byteRetorno = sockTCP.recv(BUFFER_SIZE)
+        if not byteRetorno:  # servidor fechou a conexão
+            print("Servidor encerrou a conexão.")
+            break
 
-      print (f'ECHO RECEBIDO {tuplaRetorno}: {byteRetorno.decode(CODE_PAGE)} ')
+        print(f'ECHO RECEBIDO: {byteRetorno.decode(CODE_PAGE)}')
 
-      # verificando mensagem de finalização
-      byteRetorno, tuplaRetorno = sockUDP.recvfrom(BUFFER_SIZE)
+        # Verificando se o servidor pediu encerramento
+        if byteRetorno.decode(CODE_PAGE) == "EXIT_BYE":
+            print("Encerrando aplicação por ordem do servidor...")
+            break
 
-      if byteRetorno.decode(CODE_PAGE) == "EXIT_BYE":
-         print("Encerrando aplicação por ordem do servidor...")
-         break
-   except:
-      print("\nOcorreu um erro inesperado")
+    except Exception as e:
+        print("\nOcorreu um erro inesperado:", e)
+        break
+
 # Fechando o socket
-sockUDP.close()
+sockTCP.close()
